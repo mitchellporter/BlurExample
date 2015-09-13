e143180b473e1faadb473b7e5365309e41dec36e
Mathew Sanders <mathew.sanders@gmail.com>
2015-09-12 22:31:12 -0400

    Example of using a blur effect for a background of a modal view using storyboards.

---
+++ BlurExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BlurExample
+//
+//  Created by Mathew Sanders on 9/12/15.
+//  Copyright © 2015 Mat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func close(segue: UIStoryboardSegue) {
+        
+    }
+
+}
+
